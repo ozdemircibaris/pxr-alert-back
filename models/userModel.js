12e19733bf58895c033942fe29396453cbf34e09
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2020-08-13 11:31:02 +0300

    update model

--- models/userModel.js
+++ models/userModel.js
@@ -24,6 +24,7 @@ module.exports = (sequelize, Sequelize) => {
         },
         phoneToken: {
             type: Sequelize.STRING,
+            unique: true,
             allowNull: false
         }
     },
