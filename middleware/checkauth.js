5af11d65751e3d61b4fd0aaab250dbbc800f80aa
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2020-09-04 09:02:55 +0300

    checkout process

--- middleware/checkauth.js
+++ middleware/checkauth.js
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
         next();
     }catch(error) {
         return res.status(401).send({
-            message: 'Auth failed'
+            message: 'UnAuthenticated'
         });
     }
 }
